f5f9879f5010ff1d2093db86ff78447ca97b1418
Mariep441 <51992909+Mariep441@users.noreply.github.com>
2019-12-15 17:09:58 +0100

    Create Button.py

---
+++ Button.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import time
+
+from wia import Wia
+from sense_hat import SenseHat
+sense = SenseHat()
+
+wia = Wia()
+wia.access_token ="d_sk_gmnO6Rpy0Pfnlpawq8NWWbfn"
+
+
+while True:
+  for event in sense.stick.get_events():
+      if event.action == "pressed":
+          sense.show_letter("R")
+          wia.Event.publish(name="button",data="start recording")
+          time.sleep(0.5)
+          sense.clear()
+          exit()
+False
+
+exit()
